946244ea3a181a2719fc87d8e22b0d9eb908ab05
KKDevelop <kkdevs@mail.ru>
2020-04-16 17:19:09 +1000

    Bump to 3.0.0-SNAPSHOT Check for FormAPI added

--- src/main/java/idk/plugin/npc/Loader.java
+++ src/main/java/idk/plugin/npc/Loader.java
@@ -2,6 +2,7 @@ package idk.plugin.npc;
 
 import cn.nukkit.Server;
 import cn.nukkit.plugin.PluginBase;
+import cn.nukkit.plugin.PluginManager;
 import idk.plugin.npc.commands.NpcCommand;
 import idk.plugin.npc.listeners.entity.EntityDamageListener;
 import idk.plugin.npc.listeners.entity.EntityVehicleEnterListener;
@@ -15,6 +16,12 @@ public class Loader extends PluginBase {
     @Override
     public void onEnable() {
         NPC.registerEntity();
+        PluginManager pluginManager = Server.getInstance().getPluginManager();
+        if(pluginManager.getPlugin("FormAPI") == null) {
+            getLogger().alert("§cRequired component not found (FormAPI Plugin)");
+            pluginManager.disablePlugin(this);
+            return;
+        }
         this.registerListeners();
         this.registerCommands();
     }
